1c76b7357a86f601008e2c0faba3ba77e85ba0ad
ipan-gunawan <ipangunawan2308@gmail.com>
2025-08-15 09:08:03 +0700

    Update

--- app/components/MainActivity.tsx
+++ app/components/MainActivity.tsx
@@ -4,7 +4,7 @@ const MainActivity = () => {
   return (
     <div className="flex justify-center flex-col border-b p-1.5 border-b-primary-content mb-4 pt-15 w-full">
       <h2 className="text-4xl text-primary-content font-bold mb-5 text-wrap">
-        What do you want to do today?
+        Deswidut bawu, can mandi
       </h2>
       <div className="your-task flex justify-between">
         <h3 className="text-xl">Here are your task!</h3>
